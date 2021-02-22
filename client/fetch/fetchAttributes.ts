3494a08819929dbb4e546f49e7bde5cffe77dd45
r.batrshin@russianit.ru <r.batrshin@russianit.ru>
2021-02-22 11:17:20 +0400

    [ADD] добавлен тип атрибута

--- client/fetch/fetchAttributes.ts
+++ client/fetch/fetchAttributes.ts
@@ -1,14 +1,13 @@
 import {API} from "../api";
+import {Atrribute} from "../types";
 
-export default async function FetchAttributes() {
+export default async function FetchAttributes(): Promise<Atrribute | []> {
     try {
         const responseAttributes = await fetch(process.env.APP_URL + API.ATTRIBUTES.url, {...API.ATTRIBUTES.options});
         const attributes = await responseAttributes.json();
 
-        return  attributes.data;
-    }
-
-    catch (err) {
+        return attributes.data;
+    } catch (err) {
         return [];
     }
 }
---
+++ client/types.ts
@@ -0,0 +1,9 @@
+
+export type Atrribute = {
+    id: Number,
+    name: String,
+    slug: String,
+    type: String,
+    order_by: String,
+    has_acrhives: Boolean,
+}
